623e9314d2acebeabd6326fd5d3065dae6b1e943
antopfs <antopfs@gmail.com>
2022-10-08 16:24:14 -0300

    com 2

--- lighthouse.js
+++ lighthouse.js
@@ -4,8 +4,8 @@ const chromeLauncher = require('chrome-launcher');
 
 (async () => {
   const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
-  const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance', 'seo', 'accessibility','best practice'], port: chrome.port};
-  const runnerResult = await lighthouse('http://localhost/lighthouseNodeModule', options);
+  const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance', 'seo', 'accessibility','best-practices'], port: chrome.port, device:'desktop'};
+  const runnerResult = await lighthouse('http://localhost/lighthouseNodeModule/', options);
 
   // `.report` is the HTML report as a string
   const reportHtml = runnerResult.report;
@@ -14,6 +14,10 @@ const chromeLauncher = require('chrome-launcher');
   // `.lhr` is the Lighthouse Result as a JS object
   console.log('Report is done for', runnerResult.lhr.finalDisplayedUrl);
   console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+  console.log('acesibilidad score was', runnerResult.lhr.categories.accessibility.score * 100);
+  console.log('seo score was', runnerResult.lhr.categories.seo.score * 100);
+  // console.log('seo score was', runnerResult.lhr);
+
 
   await chrome.kill();
 })();
